1654f84cbb7cb1f9e6b3644cde5e1ca471dbd0e9
Suhwan Hwang <entercritical@gmail.com>
2020-04-12 23:19:10 +0900

    codeforces r633 a

---
+++ codeforces/r633/problem1/main.cpp
@@ -0,0 +1,58 @@
+// from bits/stdc++.h
+// C
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <regex>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int solve(int n) {
+  return n;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t-- > 0) {
+    int n;
+    cin >> n;
+    cout << solve(n) << endl;
+  }
+  return 0;
+}
