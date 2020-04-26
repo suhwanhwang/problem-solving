4b4c2669f9156a627dcb766b9dd96a813f52b808
Suhwan Hwang <entercritical@gmail.com>
2020-04-27 01:33:41 +0900

    codeforces e086 c wrong

---
+++ codeforces/e086/problem3/main.cpp
@@ -0,0 +1,78 @@
+// from bits/stdc++.h
+// C
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <regex>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+ll gcd(ll a, ll b) {
+	return b ? gcd(b, a%b) : a;
+}
+
+void solve(ll a, ll b, ll q) {
+  if (a > b) {
+    swap(a, b);
+  }
+  ll lcm = a * b / gcd(a, b);
+  for (int i = 0; i < q; ++i) {
+    ll l, r;
+    cin >> l >> r;
+
+    if (a == b) {
+      cout << 0;
+    } else if (r < lcm) {
+      cout << (r - b + 1 < 0 ? 0 : r - b + 1);
+    } else {
+      cout << (r - l + 1) - ((r - l) / lcm) * b;
+    }
+    cout << (i == q-1 ? '\n' : ' ');
+  }
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+
+  int t;
+  cin >> t;
+  while (t-- > 0) {
+    ll a, b, q;
+    cin >> a >> b >> q;
+    solve(a, b, q);
+  }
+  return 0;
+}
