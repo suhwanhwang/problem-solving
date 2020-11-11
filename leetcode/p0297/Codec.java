83d225134d2978772f71708b1ef140ef460da08e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2020-11-11 20:34:07 +0900

    Update Codec.java

--- leetcode/p0297/Codec.java
+++ leetcode/p0297/Codec.java
@@ -1,60 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Codec {
 
-    private void subSerialize(TreeNode node, int index, Map<Integer, Integer> map) {
-        map.put(index, node == null ? null : node.val);
-        if (node == null) {
-            return;
-        }
-        
-        subSerialize(node.left, index*2+1, map);
-        subSerialize(node.right, index*2+2, map);     
-    }
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null) return "";
+        if (root == null) {
+            return "";
+        }
         
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        
-        subSerialize(root, 0, map);
+        Queue<TreeNode> q = new LinkedList<>();
         
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i <= map.lastKey(); ++i) {
-            Integer value = map.get(i);
-            sb.append(value);
+        
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                sb.append("null,");
+                continue;
+            }
+            
+            sb.append(node.val);
             sb.append(",");
+            q.offer(node.left);
+            q.offer(node.right);
         }
+        
         if (sb.length() > 0) {
             sb.setLength(sb.length() - 1);
         }
         System.out.println(sb.toString());
         return sb.toString();
     }
-    
-    private TreeNode subDeserialize(int index, String[] token) {
-        if (index >= token.length) {
-            return null;
-        }
-        
-        String s = token[index];
-        if (s.equals("null")) {
-            return null;
-        }
-        
-        
-        TreeNode node = new TreeNode(Integer.parseInt(s));
-        node.left = subDeserialize(index * 2 + 1, token);
-        node.right = subDeserialize(index * 2 + 2, token);
-        return node;
-    }
-    
+
+
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if (data.length() == 0) {
             return null;
         }
-        
         String[] token = data.split(",");
-        TreeNode root = subDeserialize(0,token);
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(token[0]));
+        
+        q.offer(root);
+        for (int i = 1; i < token.length; i += 2) {
+            
+            TreeNode node = q.poll();
+            
+            if (token[i].equals("null")) {
+                node.left = null;
+            } else {
+                node.left = new TreeNode(Integer.parseInt(token[i]));
+                q.offer(node.left);
+            }
+            
+            
+            if (token[i+1].equals("null")) {
+                node.right = null;
+            } else {
+                node.right = new TreeNode(Integer.parseInt(token[i+1]));
+                q.offer(node.right);
+            }
+        }
+        
         return root;
     }
 }
