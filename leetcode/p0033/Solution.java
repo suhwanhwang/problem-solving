59be34b0228d5109e2a342354a1774e0e03bd1fb
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2020-11-18 08:40:39 +0900

    Update Solution.java

--- leetcode/p0033/Solution.java
+++ leetcode/p0033/Solution.java
@@ -42,7 +42,33 @@ class Solution {
         return -1;
     }
     
-    public int search(int[] nums, int target) {     
+    public int search_(int[] nums, int target) {     
         return sub(nums, target, 0, nums.length - 1);
+    }
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            //System.out.println(l +" " + m + " " + r);
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (nums[m] <= nums[r]) {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
