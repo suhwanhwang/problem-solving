e878b924c01d6243c186c1eb0229fc1749da2403
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2020-11-18 20:10:01 +0900

    Update main.cpp

--- leetcode/p0034/main.cpp
+++ leetcode/p0034/main.cpp
@@ -1,11 +1,14 @@
 /*
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
-Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+Given an array of integers nums sorted 
+in ascending order, find the starting 
+and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
 
-Follow up: Could you write an algorithm with O(log n) runtime complexity?
+Follow up: Could you write an algorithm 
+with O(log n) runtime complexity?
 
  
 
