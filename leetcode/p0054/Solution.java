380daca11a0924b38bdcd873f0f02399b45f73f4
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2020-11-28 22:09:03 +0900

    Update Solution.java

--- leetcode/p0054/Solution.java
+++ leetcode/p0054/Solution.java
@@ -1,3 +1,12 @@
+/*
+https://leetcode.com/problems/spiral-matrix/submissions/
+
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+*/
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
@@ -7,27 +16,25 @@ class Solution {
         
         int h = matrix.length;
         int w = matrix[0].length;
-        int[][] dir = { //x, y
+        int[][] dir = {
             {0, 1}, {1, 0}, {0, -1}, {-1, 0}   
         };
+        boolean[][] added = new boolean[h][w];
         
-        int cur = 0;
         int r = 0;
-        int c = -1;
-        int curW = w;
-        int curH = h-1;
-        while (ans.size() < h * w) {
-            int len = (dir[cur][0] == 0 ? curW-- : curH--);
-            //System.out.println("len = " + len);
-            for (int i = 0; i < len; ++i) {
-                r += dir[cur][0];
-                c += dir[cur][1];
-
-                //System.out.println("(" + r + "," + c + ")");
-                ans.add(matrix[r][c]);
-            }
+        int c = 0;
+        int d = 0;
+        for (int i = 0; i < h * w; ++i) {
+            ans.add(matrix[r][c]);
+            added[r][c] = true;
             
-            cur = (cur + 1) % dir.length;
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
+            if (nr < 0 || nr >= h || nc < 0 || nc >= w || added[nr][nc]) {
+                d = (d + 1) % dir.length;
+            }
+            r += dir[d][0];
+            c += dir[d][1];
         }
         
         return ans;
