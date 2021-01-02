c18e46e54818ded55db59e366038a53c0cc39b9c
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-01-02 19:19:58 +0900

    Create Solution.java

---
+++ leetcode/p0116/Solution.java
@@ -0,0 +1,66 @@
+/*
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> q = new LinkedList<>();
+        
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            int len = q.size();
+            Node prev = null;
+            for (int i = 0; i < len; ++i) {
+                Node node = q.poll();
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+                if (prev != null) {
+                    prev.next = node;
+                }
+                prev = node;
+            }
+        }
+        return root;
+    }
+}
