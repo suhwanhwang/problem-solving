e06395a15e055541132fc46a7f4045220addb5fa
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-02-27 07:49:22 +0900

    Create Solution.java

---
+++ leetcode/p0295/Solution.java
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/find-median-from-data-stream/
+
+Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+
+For example,
+[2,3,4], the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+ 
+
+Example:
+
+addNum(1)
+addNum(2)
+findMedian() -> 1.5
+addNum(3) 
+findMedian() -> 2
+ 
+
+Follow up:
+
+If all integer numbers from the stream are between 0 and 100, how would you optimize it?
+If 99% of all integer numbers from the stream are between 0 and 100, how would you optimize it?
+*/
+class MedianFinder {
+
+    /** initialize your data structure here. */
+    Queue<Integer> maxPq = new PriorityQueue<Integer>((n1, n2) -> { return -(n1 - n2); });
+    Queue<Integer> minPq = new PriorityQueue<Integer>();
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        maxPq.offer(num);
+        minPq.offer(maxPq.poll());
+        
+        if (maxPq.size() < minPq.size()) {
+            maxPq.offer(minPq.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (maxPq.size() == minPq.size()) {
+            return (maxPq.peek() + minPq.peek()) / 2.0;
+        } else {
+            return (double)maxPq.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
