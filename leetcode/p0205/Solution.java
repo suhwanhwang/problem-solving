0a9d11d9d93ac2aaa5532f79c8e2c446e89e2fc7
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-07-12 21:37:41 +0900

    Create Solution.java

---
+++ leetcode/p0205/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        return convertInt(s).equals(convertInt(t));
+    }
+    
+    private String convertInt(String s) {
+        StringBuilder sb = new StringBuilder();
+        Map<Character,Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                sb.append(map.get(c));
+            } else {
+                char converted = (char)('a' + map.size());
+                map.put(c, converted);
+                sb.append(converted);
+            }
+        }
+        return sb.toString();
+    }
+}
