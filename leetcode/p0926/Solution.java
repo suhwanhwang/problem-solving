2d95d4b8b298db0f7ff3cc158ffbecc53651e323
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-08-13 08:07:59 +0900

    Create Solution.java

---
+++ leetcode/p0926/Solution.java
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/flip-string-to-monotone-increasing/
+*/
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int[] pre = new int[s.length()+1];
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            pre[i+1] = pre[i] + (c - '0');
+        }
+        
+        int min = s.length();
+        for (int i = 0; i <= s.length(); ++i) {
+            int toZero = pre[i + 1];
+            int toOne = (s.length() - i) - (pre[s.length()] - pre[i]);
+            min = Math.min(min, toZero+toOne);
+        }
+        return min;
+    }
+}
