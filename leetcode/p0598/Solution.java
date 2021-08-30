22dbbc812fdde5d8b0bca03d65e122831c6f2def
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-08-31 06:55:02 +0900

    Create Solution.java

---
+++ leetcode/p0598/Solution.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/range-addition-ii/
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        int minM = m;
+        int minN = n;
+        for (int i = 0; i < ops.length; ++i) {
+            minM = Math.min(minM, ops[i][0]);
+            minN = Math.min(minN, ops[i][1]);
+        }
+        return minM * minN;
+    }
+}
