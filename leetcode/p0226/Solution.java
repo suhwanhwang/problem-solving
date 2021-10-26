463f0da36a034016a4b6f2b3c0fb945deb3666bf
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-10-26 12:37:51 +0900

    Create Solution.java

---
+++ leetcode/p0226/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        
+        TreeNode inverted = new TreeNode(root.val);
+        inverted.left = invertTree(root.right);
+        inverted.right = invertTree(root.left);
+        return inverted;
+    }
+}
