6339e5bc1573c50ba92d9866910d56239fa41f7f
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-11-19 08:27:16 +0900

    Update Solution.java

--- leetcode/p0448/Solution.java
+++ leetcode/p0448/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public List<Integer> findDisappearedNumbers_n(int[] nums) {
         Set<Integer> ans = new HashSet<>();
         for (int i = 1; i <= nums.length; ++i) {
             ans.add(i);
@@ -11,4 +11,22 @@ class Solution {
         
         return new ArrayList<>(ans);
     }
+    
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int num = Math.abs(nums[i]);
+            
+            if (nums[num - 1] > 0) {
+                nums[num - 1] *= -1;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+        return ans;
+    }   
 }
