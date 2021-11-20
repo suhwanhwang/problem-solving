9a7eacc97f32bf090a57f2ba49a786e79816de96
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-11-20 15:05:06 +0900

    Update Solution.java

--- leetcode/p0540/Solution.java
+++ leetcode/p0540/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+    public int singleNonDuplicate_n(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
@@ -11,5 +11,29 @@ class Solution {
         }
         
         return (nums[nums.length - 2] != nums[nums.length - 1]) ? nums[nums.length - 1] : -1;
+    }
+
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length;
+        
+        while(lo < hi) {
+            int mid = (lo + hi) >>> 1;
+            mid -= (mid%2);
+            int cur = nums[mid];
+            //System.out.println(lo + " " + mid + " " + hi);
+            int next = (mid + 1 >= nums.length) ? -1 : nums[mid+1];
+            if (cur != next) {
+                int prev =  (mid - 1 >= 0) ? nums[mid-1] : -1;
+                if (prev == cur) {
+                    hi = mid;
+                } else {
+                    return cur;
+                }
+            } else {
+                lo = mid+1;
+            }
+        }
+        return -1;
     }
 }
