e4574b86f389408b3af9a62b473549855d44340e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-11-26 06:13:31 +0900

    Create Solution.java

---
+++ leetcode/p0053/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            if (nums[i] > sum + nums[i]) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
