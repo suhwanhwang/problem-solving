1f633c466aacfd6fe8df13a25efa458114ba9ff3
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2021-12-15 05:47:26 +0900

    Update Solution.java

--- leetcode/p0938/Solution.java
+++ leetcode/p0938/Solution.java
@@ -14,43 +14,21 @@
  * }
  */
 class Solution {
-    List<Integer> list;
+    private int sum;
     public int rangeSumBST(TreeNode root, int low, int high) {
-        list = new ArrayList<>();
-        dfs(root);
-        //System.out.println(list);
-        int lowIndex = binarySearch(list, low);
-        int highIndex = binarySearch(list, high);
-        if (lowIndex == -1 || highIndex == -1) return -1;
-        
-        int sum = 0;
-        for (int i = lowIndex; i <= highIndex; ++i) {
-            sum += list.get(i);
-        }
+        sum = 0;
+        dfs(root, low, high);
         return sum;
     }
-    private void dfs(TreeNode node) {
+    private void dfs(TreeNode node, int low, int high) {
         if (node == null) return;
         
-        dfs(node.left);
-        list.add(node.val);
-        dfs(node.right);
-    }
-    private int binarySearch(List<Integer> list, int target) {
-        int low = 0;
-        int high = list.size() - 1;
+        dfs(node.left, low, high);
         
-        while(low <= high) {
-            int mid = (low + high) >>> 1;
-            int val = list.get(mid);
-            if(val == target) {
-                return mid;
-            } else if (val < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        if (low <= node.val && node.val <= high) {
+            sum += node.val;
         }
-        return -1;
+        
+        dfs(node.right, low, high);
     }
 }
