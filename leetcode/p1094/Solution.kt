81d8691ab43c71a15eb2dbaa129f40c7108b854f
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-01-08 06:42:00 +0900

    Update Solution.kt

--- leetcode/p1094/Solution.kt
+++ leetcode/p1094/Solution.kt
@@ -1,18 +1,20 @@
 class Solution {
     fun carPooling(trips: Array<IntArray>, capacity: Int): Boolean {
         val sorted = trips.sortedBy { it[1] }
-        val pq = PriorityQueue<Int>()
+        val pq = PriorityQueue<Int> { i0, i1 ->
+            sorted[i0][2] - sorted[i1][2]
+        }
+        var curCapacity = 0
         var max = 0
-        for (t in sorted) {
-            while(!pq.isEmpty() && pq.peek() <= t[1]) {
-                pq.poll()
+        for ((i, t) in sorted.withIndex()) {
+            while(!pq.isEmpty() && sorted[pq.peek()][2] <= t[1]) {
+                curCapacity -= sorted[pq.poll()][0]
             }
             
-            for (i in 0 until t[0]) {
-                pq.offer(t[2])
-            }
+            pq.offer(i)
+            curCapacity += sorted[i][0]
             
-            max = Math.max(max, pq.size);
+            max = Math.max(max, curCapacity);
         }
         return max <= capacity
     }
