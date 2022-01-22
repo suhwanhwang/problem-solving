435313d5cc5211360227ee310db25c84c5a7604e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-01-22 16:06:15 +0900

    Update Solution.java

--- leetcode/p1510/Solution.java
+++ leetcode/p1510/Solution.java
@@ -1,32 +1,26 @@
 class Solution {
-    int[] dpAlice;
-    int[] dpBob;
+    int[] dp;
     public boolean winnerSquareGame(int n) {
-        dpAlice = new int[n + 1];
-        Arrays.fill(dpAlice, -1);
-        dpBob = new int[n + 1];
-        Arrays.fill(dpBob, -1);
-        return solve(n, true);
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(n);
     }
     
-    private boolean solve(int n, boolean isAlice) {
+    private boolean solve(int n) {
         if (n == 0) {
-            return !isAlice;
+            return false;
         }
-        
-        int [] dp = (isAlice? dpAlice : dpBob);
         
         if (dp[n] != -1) {
             return dp[n] == 1;
         }
         for (int i = 1; i * i <= n; ++i) {
-            boolean ret = solve(n - i*i, !isAlice);
-            if (ret == isAlice) {
-                dp[n] = isAlice ? 1 : 0;
-                return isAlice;
+            if (!solve(n - i*i)) {
+                dp[n] = 1;
+                return true;
             }
         }
-        dp[n] = !isAlice ? 1 : 0;
-        return !isAlice;
+        dp[n] = 0;
+        return false;
     }
 }
