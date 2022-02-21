63d700a58061844a83900b26ce77ca3b1eea577d
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-02-22 08:51:02 +0900

    Update Solution.kt

--- leetcode/p0169/Solution.kt
+++ leetcode/p0169/Solution.kt
@@ -1,6 +1,23 @@
 class Solution {
-    fun majorityElement(nums: IntArray): Int {
+    fun majorityElement_nlogn(nums: IntArray): Int {
         nums.sort()
         return nums[nums.size / 2]
+    }
+    
+    fun majorityElement(nums: IntArray): Int {
+        var major = 0
+        var count = 0
+        for (num in nums) {
+            if (count == 0) {
+                major = num
+            }
+            
+            if (num == major) {
+                count++
+            } else {
+                count--
+            }
+        }
+        return major
     }
 }
