070b13ec33f48f538f2b9002be7a64ecba8ba40e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-03-13 07:02:36 +0900

    Update Solution.kt

--- leetcode/p0138/Solution.kt
+++ leetcode/p0138/Solution.kt
@@ -13,29 +13,19 @@ class Solution {
     fun copyRandomList(node: Node?): Node? {
         val nodeMap = mutableMapOf<Node, Node?>()
         
-        var dummy = Node(0)
-        var copied: Node? = dummy
         var cur = node
         while (cur != null) {
-            val copiedNode = Node(cur.`val`)
-            
-            copied?.next = copiedNode
-            nodeMap[cur] = copiedNode
-            
-            copied = copied?.next
+            nodeMap[cur] = Node(cur.`val`)            
             cur = cur.next
         }
         
         cur = node
         while (cur != null) {
-            val random = cur.random
-            if (random != null) {
-                val copiedNode = nodeMap[cur]
-                copiedNode?.random = nodeMap[random]
-            }
-            
+            val copied = nodeMap[cur]
+            copied?.next = nodeMap[cur.next]
+            copied?.random = nodeMap[cur.random]
             cur = cur.next
         }
-        return dummy.next
+        return nodeMap[node]
     }
 }
