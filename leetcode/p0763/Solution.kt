a3169dbf32dca60091bdbd6fe5a19a2032280f37
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-03-22 06:22:27 +0900

    Create Solution.kt

---
+++ leetcode/p0763/Solution.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun partitionLabels(s: String): List<Int> {
+        val lastPosition = mutableMapOf<Char, Int>()
+        for (i in s.indices) {
+            lastPosition[s[i]] = i
+        }
+        
+        val ans = mutableListOf<Int>()
+        var start = 0
+        while(start < s.length) {
+            var last = lastPosition[s[start]] ?: -1
+            var i = start + 1
+            while (i < last) {
+                last = Math.max(last, lastPosition[s[i]] ?: -1)
+                i++
+            }
+            ans.add(last - start + 1)
+            start = last + 1
+        }
+        
+        return ans
+    }
+}
