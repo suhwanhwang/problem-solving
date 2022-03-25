999f9491555710607e996aa4b0a0e6c5c2e09dfb
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-03-26 05:42:53 +0900

    Create Solution.kt

---
+++ leetcode/p1029/Solution.kt
@@ -0,0 +1,27 @@
+class Solution {
+    val INF = 987654321
+    val dp = Array<IntArray>(100 + 1) { IntArray(50 + 1) { 0 } }
+    
+    fun twoCitySchedCost(costs: Array<IntArray>): Int {
+        return helper(costs, 0, 0)
+    }
+    
+    private fun helper(costs: Array<IntArray>, index: Int, countA: Int): Int {
+        if (index >= costs.size) {
+            return if (countA != costs.size/2) INF else 0
+        }
+        
+        if (dp[index][countA] != 0) {
+            return dp[index][countA]
+        }
+        
+        var minCost = INF
+        if (countA < costs.size / 2) {
+            minCost = costs[index][0] + helper(costs, index + 1, countA + 1)
+        }
+        
+        minCost = Math.min(minCost, costs[index][1] + helper(costs, index + 1, countA))
+        dp[index][countA] = minCost
+        return minCost
+    }
+}
