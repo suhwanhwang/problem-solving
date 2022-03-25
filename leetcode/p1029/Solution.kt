8b0e455fa57c7e21b222420c212c452c1776668f
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-03-26 06:37:33 +0900

    Update Solution.kt

--- leetcode/p1029/Solution.kt
+++ leetcode/p1029/Solution.kt
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     val INF = 987654321
     val dp = Array<IntArray>(100 + 1) { IntArray(50 + 1) { 0 } }
     
@@ -23,5 +24,19 @@ class Solution {
         minCost = Math.min(minCost, costs[index][1] + helper(costs, index + 1, countA))
         dp[index][countA] = minCost
         return minCost
+    }
+    */
+    
+    fun twoCitySchedCost(costs: Array<IntArray>): Int {
+        costs.sortBy { it[0] - it[1] }
+        
+        var sum = 0
+        for (i in 0 until costs.size / 2) {
+            sum += costs[i][0]
+        }
+        for (i in costs.size / 2 until costs.size) {
+            sum += costs[i][1]
+        }
+        return sum
     }
 }
