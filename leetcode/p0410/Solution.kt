22d9d44abed81ac08c312591f3286611a038e6dc
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-04-01 06:19:17 +0900

    Update Solution.kt

--- leetcode/p0410/Solution.kt
+++ leetcode/p0410/Solution.kt
@@ -1,13 +1,17 @@
 class Solution {
     val dp = Array(51) { IntArray(1001) {-1} }
-    
+    var prefixSum = IntArray(1000001) {0}
     fun splitArray(nums: IntArray, m: Int): Int {
+        prefixSum[0] = nums[0]
+        for(i in 1..nums.size) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1]
+        }
         return helper(nums, m, 0)
     }
     
     private fun helper(nums: IntArray, m: Int, start: Int): Int {
         if (m == 1) {
-            return nums.filterIndexed { index, value -> index >= start }.sum()
+            return prefixSum[nums.size] - prefixSum[start]
         }
         
         if (dp[m][start] != -1) {
@@ -17,9 +21,7 @@ class Solution {
         var sum = 0
         var min = Int.MAX_VALUE
         for (i in start..(nums.size - m)) {
-            if (nums[i] == 0 && min != Int.MAX_VALUE) {
-                continue
-            }
+        
             sum += nums[i]
             val nextSum = helper(nums, m - 1, i + 1)
             val curMax = Math.max(sum, nextSum)
