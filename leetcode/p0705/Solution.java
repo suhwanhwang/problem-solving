53b4761f03066f0186a29bb57217e0364d76420e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-04-21 15:34:17 +0900

    Create Solution.java

---
+++ leetcode/p0705/Solution.java
@@ -0,0 +1,53 @@
+class MyHashSet {
+    static class Node {
+        List<Integer> list = new ArrayList<>();
+    }
+    
+    private static final int TABLE_SIZE = 10000;
+    private Node[] table = new Node[TABLE_SIZE + 1];
+    
+    public MyHashSet() {
+        for (int i = 0; i < table.length; ++i) {
+            table[i] = new Node();
+        }
+    }
+    
+    public void add(int key) {
+        if (contains(key)) {
+            return;
+        } else {
+            table[getHash(key)].list.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        Node node = getNode(key);
+        node.list.remove(Integer.valueOf(key));
+    }
+    
+    public boolean contains(int key) {
+        Node node = getNode(key);
+        for (int k : node.list) {
+            if (k == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private Node getNode(int key) {
+        return table[getHash(key)];
+    }
+    
+    private int getHash(int key) {
+        return key % TABLE_SIZE;    
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
