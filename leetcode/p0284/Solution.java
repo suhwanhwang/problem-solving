98b78e3284b4c9659cee8914fb6280479815bb91
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-04-25 20:08:20 +0900

    Create Solution.java

---
+++ leetcode/p0284/Solution.java
@@ -0,0 +1,37 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> iterator;
+    Integer next = null;
+    public PeekingIterator(Iterator<Integer> iterator) {
+	// initialize any member here.
+	this.iterator = iterator;
+        if (iterator.hasNext()) {
+            next = iterator.next();
+        }
+    }
+	
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+	
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer cur = next;
+        if (iterator.hasNext()) {
+            next = iterator.next();
+        } else {
+            next = null;
+        }
+        return cur;
+    }
+	
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+}
