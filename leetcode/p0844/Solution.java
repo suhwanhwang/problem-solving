caaba905757cb5acf16638fcfdd4484b394b2dd2
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-05-01 09:36:49 +0900

    Create Solution.java

---
+++ leetcode/p0844/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return convert(s).equals(convert(t));
+    }
+    
+    private String convert(String s) {
+         StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if (c != '#') {
+                sb.append(c);
+            } else {
+                if (sb.length() > 0)               sb.deleteCharAt(sb.length()-1);
+            }
+        }
+        return sb.toString();
+    }
+}
