49a736b17dda1be506fc0f1a89f23d710e859b68
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-06-10 18:02:09 +0900

    Update Solution.kt

--- leetcode/p0003/Solution.kt
+++ leetcode/p0003/Solution.kt
@@ -1,19 +1,54 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
 class Solution {
-    fun lengthOfLongestSubstring(s: String): Int {
-        val set = mutableSetOf<Char>()
-        var max = 0
-        var left = 0;
-        var right = 0;
-        while (left <= right && right < s.length) {
-            if (set.contains(s[right])) {
-                set.remove(s[left])
-                left++
-            } else {
-                set.add(s[right])
-                right++
-                max = Math.max(max, set.size)
-            }
+    fun getIntersectionNode_on(headA:ListNode?, headB:ListNode?):ListNode? {
+        
+        val set = mutableSetOf<ListNode>()
+        var cur = headA
+        while (cur != null) {
+            set.add(cur)
+            cur = cur.next
         }
-        return max
+        
+        cur = headB
+        while (cur != null) {
+            if (set.contains(cur)) {
+                return cur
+            }
+            cur = cur.next
+        }
+        return null
+    }
+    
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        var hA = headA
+        var hB = headB
+        var curA = hA
+        var curB = hB
+        
+        while (curA != null && curB != null && curA != curB) {
+           if (curA.next != null) {
+               curA = curA.next
+           } else {
+               curA = hB
+               hB = null
+           }
+           
+           if (curB.next != null) {
+               curB = curB.next
+           } else {
+               curB = hA
+               hA = null
+           }
+        }
+        return curA
     }
 }
