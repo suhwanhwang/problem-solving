b3bb35ff7af0942c59095b15471affcde6d44a8f
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-08-12 09:10:32 +0900

    Update Solution2.java

--- leetcode/p0098/Solution2.java
+++ leetcode/p0098/Solution2.java
@@ -1,9 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
-    private boolean isValidBST(TreeNode root, int left, int right) {
+    private boolean isValidBST(TreeNode root, long left, long right) {
         if (root == null) {
             return true;
         }
@@ -11,10 +26,7 @@ class Solution {
         if (!(left <= root.val && root.val <= right)) {
             return false;
         }
-        if (!(root.left == null || root.left.val < root.val) || !(root.right == null || root.val < root.right.val)) {
-            return false;
-        }
         
-        return isValidBST(root.left, left, root.val - 1) && isValidBST(root.right, root.val+1, right);
+        return isValidBST(root.left, left, (long)root.val - 1) && isValidBST(root.right, (long)root.val + 1, right);
     }
 }
