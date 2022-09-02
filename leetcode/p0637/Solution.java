979fe53691558c551408e1138790b29b808b069e
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-09-02 18:42:55 +0900

    Create Solution.java

---
+++ leetcode/p0637/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> average = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {            
+            int len = queue.size();
+            double sum = 0;
+            
+            for (int i = 0; i < len; ++i) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            average.add(sum / len);
+        }
+        return average;
+    }
+}
