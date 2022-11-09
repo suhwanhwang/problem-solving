48d9bb1e93e1b6c726485de38582239c7ab234d9
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-11-10 00:01:16 +0900

    Update Solution.java

--- leetcode/p0901/Solution.java
+++ leetcode/p0901/Solution.java
@@ -1,3 +1,4 @@
+/*
 class StockSpanner {
     private List<Integer> stock = new ArrayList<>();
 
@@ -17,5 +18,27 @@ class StockSpanner {
             }
         }
         return count;
+    }
+}
+*/
+class StockSpanner {
+    private Stack<Integer> stock = new Stack<>();
+    private Stack<Integer> count = new Stack<>();
+
+    public StockSpanner() {
+        
+    }
+    
+    public int next(int price) {
+        int sum = 1;
+        
+        while (!stock.isEmpty() && stock.peek() <= price) {
+            stock.pop();
+            sum += count.pop();
+        }
+        
+        stock.push(price);
+        count.push(sum);
+        return sum;
     }
 }
