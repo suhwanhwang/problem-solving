b7e32298f1c54f9adfc0e542e5eba5595924026d
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-11-10 20:43:19 +0900

    Create Solution2.java

---
+++ leetcode/p1047/Solution2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.insert(0, st.pop());
+        }
+        return sb.toString();
+    }
+}
