6bb360539e9051860128ee3aaadfffd168e60793
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-11-16 09:32:42 +0900

    Create Solution2.java

---
+++ leetcode/p0374/Solution2.java
@@ -0,0 +1,27 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            int guess = guess(mid);
+            if (guess == 0) {
+                return mid;
+            } else if (guess > 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
