10fb6352d4879b2f174d830b3487118e52fc5662
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2022-11-29 15:25:47 +0900

    Create Solution3.java

---
+++ leetcode/p0380/Solution3.java
@@ -0,0 +1,46 @@
+class RandomizedSet {
+    private final Map<Integer, Integer> map = new HashMap<>();
+    private final List<Integer> list = new ArrayList<>();
+    private final Random random = new Random();
+
+    public RandomizedSet() {
+        
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+        if (index != list.size() - 1) {
+            int lastVal = list.get(list.size() - 1);
+            list.set(index, lastVal);
+            map.put(lastVal, index);
+        }
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
