846208f733034c7c198c081c30b3883256df7f95
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-02-20 13:15:14 +0900

    Create Solution.java

---
+++ leetcode/p0035/20220220/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    /*
+      0 1 2 3
+      1 3 5 6
+left          ^
+right       ^
+mid         ^ 
+
+    */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
