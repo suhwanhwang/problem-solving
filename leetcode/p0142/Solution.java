0e064de64d29f706ababcb0fc6b4ecf9283f7142
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-03-10 06:51:33 +0900

    Update Solution.java

--- leetcode/p0142/Solution.java
+++ leetcode/p0142/Solution.java
@@ -8,9 +8,35 @@
  *         next = null;
  *     }
  * }
+ 0 1 2 3 4 5 6 7 8 9 10
+ 3 2 0 4 2 0 4 2 0 4 2
+
+ 0 2  3 0
+ 1 4  2 2 x
+ 2 6  0 4
+ 3 8  4 0
+ 4 10 2 2 x
+
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+    public ListNode detectCycle_(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         ListNode cur = head;
         while(cur != null) {
