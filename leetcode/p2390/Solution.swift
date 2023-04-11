475966adcc8b4683b1ee3d2bce80502e6402b575
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-04-11 16:47:03 +0900

    Create Solution.swift

---
+++ leetcode/p2390/Solution.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func removeStars(_ s: String) -> String {
+        var stack = [Character]()
+
+        for c in s {
+            if c == "*" {
+                stack.removeLast()
+            } else {
+                stack.append(c)
+            }
+        }
+
+        return String(stack)
+    }
+}
