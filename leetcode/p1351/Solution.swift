e64ed49c19361ecee04a50eec8cdc0e102a840c1
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-06-09 10:23:25 +0900

    Update Solution.swift

--- leetcode/p1351/Solution.swift
+++ leetcode/p1351/Solution.swift
@@ -1,11 +1,36 @@
 class Solution {
-    func countNegatives(_ grid: [[Int]]) -> Int {
+    /*
+    [
+        [ 4, 3, 2,-1],
+        [ 3, 2, 1,-1],
+        [ 1, 1,-1,-2],
+        [-1,-1,-2,-3]
+    ]
+    */
+    func countNegatives_nm(_ grid: [[Int]]) -> Int {
         var count = 0
         for gr in grid {
             for g in gr {
                 if g < 0 {
                     count += 1
                 }
+            }
+        }
+        return count
+    }
+
+    func countNegatives(_ grid: [[Int]]) -> Int {
+        let n = grid.count
+        let m = grid[0].count
+        var count = 0
+        var row = grid.count - 1
+        var col = 0
+        while row >= 0 && col < m {
+            if grid[row][col] >= 0 {
+                col += 1
+            } else {
+                count += (m - col)
+                row -= 1
             }
         }
         return count
