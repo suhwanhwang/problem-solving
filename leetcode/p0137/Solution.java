1a5eaee69eab694fab696d399dd23bd22f0d23e5
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-07-04 20:07:35 +0900

    Update Solution.java

--- leetcode/p0137/Solution.java
+++ leetcode/p0137/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += (num >> i) & 0x1;
+            }
+            sum %= 3;
+            ans |= (sum << i);
+        }
+        return ans;
+    }
+    /*
+    public int singleNumber(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
@@ -21,4 +35,5 @@ class Solution {
         }
         return prev;
     }
+    */
 }
