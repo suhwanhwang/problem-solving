ba6c54a7afe09b040284af8263e127939026e3a5
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-08-20 13:33:17 +0900

    Update Solution.java

--- leetcode/p518/Solution.java
+++ leetcode/p518/Solution.java
@@ -9,11 +9,8 @@ class Solution {
     }
     
     private int helper(int remain, int[] coins, int prevIndex) {
-        if (remain < 0) {
-            return 0;
-        }
-        
         if (remain == 0) {
+            dp[remain][prevIndex] = 1;
             return 1;
         }
         
@@ -23,6 +20,9 @@ class Solution {
         
         int sum = 0;
         for (int i = prevIndex; i < coins.length; ++i) {
+            if (remain < coins[i]) {
+                continue;
+            }
             sum += helper(remain - coins[i], coins, i);
         }
         dp[remain][prevIndex] = sum;
