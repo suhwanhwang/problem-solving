c9598d0b7949e615d01bde5f9a9670d3f40a0140
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-10-03 14:35:49 +0900

    Update Solution.java

--- leetcode/p1512/Solution.java
+++ leetcode/p1512/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numIdenticalPairs(int[] nums) {
+    public int numIdenticalPairs_n2(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length - 1; ++i) {
             for (int j = i + 1; j < nums.length; ++j) {
@@ -7,6 +7,19 @@ class Solution {
                     count++;
                 }
             }
+        }
+        return count;
+    }
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int count = 0;
+        for (int c : map.values()) {
+            count += c * (c - 1) / 2;
         }
         return count;
     }
