79353a0438c370271a5abb939a0c5573ac48089d
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2023-11-29 20:16:42 +0900

    Create Solution.kt

---
+++ leetcode/p0191/Solution.kt
@@ -0,0 +1,14 @@
+class Solution {
+    // you need treat n as an unsigned value
+    fun hammingWeight(n:Int):Int {
+        var count = 0
+        var num = n
+        while (num != 0) {
+            if (num and 1 == 1) {
+                count++
+            }
+            num = (num ushr 1)
+        }
+        return count
+    }
+}
