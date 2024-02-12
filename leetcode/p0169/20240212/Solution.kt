d7ffccb4cc0e1169931c2e7965f89106f9d69be3
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2024-02-12 10:33:53 +0900

    Update Solution.kt

--- leetcode/p0169/20240212/Solution.kt
+++ leetcode/p0169/20240212/Solution.kt
@@ -2,4 +2,19 @@ class Solution {
     fun majorityElement(nums: IntArray): Int {
         return nums.sorted()[nums.size / 2]
     }
+
+    fun majorityElement_n(nums: IntArray): Int {
+        var count = 0
+        var major = 0
+        
+        for (num in nums) {
+            if(count == 0) {
+                major = num
+            }
+            
+            if (num == major) count++
+            else count--
+        }
+        return major
+    }
 }
