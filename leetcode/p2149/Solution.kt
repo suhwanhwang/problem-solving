566332767ceb6c2ee05a5eb08f9f1f7c4c0b6d46
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2024-02-14 20:17:20 +0900

    Update Solution.kt

--- leetcode/p2149/Solution.kt
+++ leetcode/p2149/Solution.kt
@@ -1,5 +1,5 @@
 class Solution {
-    fun rearrangeArray(nums: IntArray): IntArray {
+    fun rearrangeArray_(nums: IntArray): IntArray {
         val pos = nums.filter { it > 0 }
         val neg = nums.filter { it < 0 }
         val ans = IntArray(nums.size)
@@ -7,6 +7,21 @@ class Solution {
         for (i in 0 until nums.size step 2) {
             ans[i] = pos[i/2]
             ans[i+1] = neg[i/2]
+        }
+        
+        return ans
+    }
+
+    fun rearrangeArray(nums: IntArray): IntArray {
+        val ans = IntArray(nums.size)
+        var pos = 0
+        var neg = 0
+        for (i in 0 until nums.size step 2) {
+            while(nums[pos] < 0) pos++
+            ans[i] = nums[pos++]
+            
+            while(nums[neg] > 0) neg++
+            ans[i+1] = nums[neg++]
         }
         
         return ans
