cdef911ebeaecebf0a96302302e3c24b7aaafc13
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2024-03-23 12:27:02 +0900

    Update Solution.kt

--- leetcode/p0143/20240323/Solution.kt
+++ leetcode/p0143/20240323/Solution.kt
@@ -7,33 +7,51 @@
  *     var next: ListNode? = null
  * }
  */
+
+/*
+     d -> 1 -> 2 -> 3 <- 4 <- 5
+pre                      ^
+first               ^
+second              ^
+
+     d -> 1 -> 5 -> 2 -> 4 -> 3 -> null
+ */
 class Solution {
     fun reorderList(head: ListNode?): Unit {
+        // 1. mid
         val mid = getMid(head)
+
+        // 2. reverse
         val reversed = reverse(mid)
-        
+
+        // 3. merge
         val dummy = ListNode(0)
-        var prev = dummy
+        dummy.next = head
+
+        var pre = dummy
         var first = head
         var second = reversed
-        
         while (first != null && second != null) {
             val firstNext = first.next
             val secondNext = second.next
-            
-            prev.next = first
+
+            pre.next = first
             first.next = second
-            prev = second
             second.next = null
-            
+
+            pre = second
             first = firstNext
             second = secondNext
         }
-        if (second != null) {
-            prev.next = second
-        }
     }
-    
+
+/*
+
+      1 -> 2 -> 3 -> 4 -> 5 -> null
+slow            ^
+fast                      ^
+
+ */
     private fun getMid(head: ListNode?): ListNode? {
         var slow = head
         var fast = head
@@ -43,16 +61,22 @@ class Solution {
         }
         return slow
     }
-    
+
+/*
+
+    <- 1 <- 2 <- 3 <- 4 -> null
+pre                   ^
+cur                        ^
+*/
     private fun reverse(head: ListNode?): ListNode? {
-        var prev: ListNode? = null
+        var pre: ListNode? = null
         var cur = head
-        while(cur != null) {
+        while (cur != null) {
             val next = cur.next
-            cur.next = prev
-            prev = cur
+            cur.next = pre
+            pre = cur
             cur = next
         }
-        return prev
+        return pre
     }
 }
