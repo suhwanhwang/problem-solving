e52a2faab9be89ec0746b57b5a36b7a9cef1940f
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2024-06-01 03:41:14 +0900

    Create Solution.kt

---
+++ leetcode/p0260/20240531/Solution.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun singleNumber(nums: IntArray): IntArray {
+        val set = mutableSetOf<Int>()
+        for (num in nums) {
+            if (set.contains(num)) {
+                set.remove(num)
+            } else {
+                set.add(num)
+            }
+        }
+        return set.toIntArray()
+    }
+}
