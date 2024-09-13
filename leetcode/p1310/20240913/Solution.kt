f15027709c2c58cd5086a6a58969f8923412475c
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2024-09-13 19:45:30 +0900

    Update Solution.kt

--- leetcode/p1310/20240913/Solution.kt
+++ leetcode/p1310/20240913/Solution.kt
@@ -1,18 +1,30 @@
 class Solution {
     fun xorQueries(arr: IntArray, queries: Array<IntArray>): IntArray {
+        val cumulative = mutableListOf<Int>()
+        var cur = 0
+        for (num in arr) {
+            cur = cur xor num
+            cumulative.add(cur)
+        }
+        
+        
         val ans = mutableListOf<Int>()
         
         for (query in queries) {
-            ans.add(getXOR(arr, query[0], query[1]))
+            val left = if (query[0] == 0) 0 else cumulative[query[0] - 1]
+            val right = cumulative[query[1]]
+            
+            ans.add(left xor right)
         }
         return ans.toIntArray()
     }
-    
-    private fun getXOR(arr: IntArray, left: Int, right: Int): Int {
-        var ans = 0
-        for (i in left..right) {
-            ans = ans xor arr[i]
-        }
-        return ans
-    }
 }
+
+/*
+0 0001  0001(1)
+1 0011  0010(2)
+2 0100  0110(6)
+3 1000  1110(7)
+
+0001 xor 0110 0111
+*/
