ed3e1d49a771b46eddd201c82f19498285587c48
Suhwan Hwang <40444306+suhwanhwang@users.noreply.github.com>
2026-04-15 19:22:38 +0900

    Optimize closestTarget method to return early

--- leetcode/p2515/20260415/Solution.kt
+++ leetcode/p2515/20260415/Solution.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun closestTarget(words: Array<String>, target: String, startIndex: Int): Int {
-        var minDist = Int.MAX_VALUE
-
         for (i in 0 until words.size) {
-            val foward = (startIndex + i) % words.size
+            val forward = (startIndex + i) % words.size
             val backward = (startIndex - i + words.size) % words.size
-            if (words[foward] == target || words[backward] == target) {
-                minDist = min(minDist, i)
+            if (words[forward] == target || words[backward] == target) {
+                return i
             }
         }
-        return if (minDist == Int.MAX_VALUE) -1 else minDist
+        return -1
     }
 }
